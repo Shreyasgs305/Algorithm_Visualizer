9d52315c6087f24ad4409ae103b5b1be3be70d69
Shreyas G S <shreyasgs305@gmail.com>
2026-05-14 15:22:02 +0530

    Fixed Vercel build

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import HomePage from "./pages/Home/HomePage";
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import Hero from "./Hero";
-import Algorithms from "./algorithms";
-import NavBar from "../../components/NavBar";
-import Footer from "../../components/Footer";
 
 function HomePage() {
   return (
